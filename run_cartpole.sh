98dba9a353eb9f5e46b3e3113c7eb7b96993d2ff
ermekaitygulov <ermek.imankulov@gmail.com>
2020-04-16 17:58:29 +0300

    1 seed; 50 epoch

--- run_cartpole.sh
+++ run_cartpole.sh
@@ -1,10 +1,10 @@
 
 REWARD_COEFFS="0.0 0.2 0.4 0.6 0.8 1.0"
-SEEDS="0 1 2 3 4"
+SEEDS="0"
 for REWARD_COEFF in $REWARD_COEFFS
 do
     for SEED in $SEEDS
     do
-	    python3 run_mujoco.py --env_id $1 --seed $SEED --num_epochs 200 --reward_coeff $REWARD_COEFF --expert_path $2
+	    python3 run_mujoco.py --env_id $1 --seed $SEED --num_epochs 50 --reward_coeff $REWARD_COEFF --expert_path $2
     done
 done
