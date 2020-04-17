67a76db21938774a2e290bc5b886df52e342df0c
ermekaitygulov <ermek.imankulov@gmail.com>
2020-04-17 11:08:30 +0300

    Revert "use log_dir arg in run_mujoco.py"
    This reverts commit 8938d158

--- run_cartpole.sh
+++ run_cartpole.sh
@@ -5,6 +5,6 @@ for REWARD_COEFF in $REWARD_COEFFS
 do
     for SEED in $SEEDS
     do
-	    python3 run_mujoco.py --env $1 --seed $SEED --num_epochs 50 --reward_coeff $REWARD_COEFF --expert_path $2 --log_dir train/
+	    python3 run_mujoco.py --env_id $1 --seed $SEED --num_epochs 50 --reward_coeff $REWARD_COEFF --expert_path $2 --log_dir train/
     done
 done
