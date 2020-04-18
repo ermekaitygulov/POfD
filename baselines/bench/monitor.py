de391368444dfa2a7809faa1b395225f77dab0bd
ermekaitygulov <ermek.imankulov@gmail.com>
2020-04-18 15:47:42 +0300

    add wandb_log attribute to Monitor wrapper to disable it in play mode

--- baselines/bench/monitor.py
+++ baselines/bench/monitor.py
@@ -33,7 +33,8 @@ class Monitor(Wrapper):
         self.episode_lengths = []
         self.episode_times = []
         self.total_steps = 0
-        self.current_reset_info = {} # extra info about the current episode, that was passed in during reset()
+        self.current_reset_info = {}
+        self.wandb_log = True# extra info about the current episode, that was passed in during reset()
 
     def reset(self, **kwargs):
         self.reset_state()
@@ -65,7 +66,8 @@ class Monitor(Wrapper):
             eprew = sum(self.rewards)
             eplen = len(self.rewards)
             epinfo = {"r": round(eprew, 6), "l": eplen, "t": round(time.time() - self.tstart, 6)}
-            wandb.log({"reward": round(eprew, 6), "episode": len(self.episode_rewards)})
+            if self.wandb_log:
+                wandb.log({"reward": round(eprew, 6), "episode": len(self.episode_rewards)})
             for k in self.info_keywords:
                 epinfo[k] = info[k]
             self.episode_rewards.append(eprew)
