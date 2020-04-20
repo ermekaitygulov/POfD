cf706507bb3fdf8c89030e557b5d7c455b23fbac
ermekaitygulov <ermek.imankulov@gmail.com>
2020-04-20 12:11:39 +0300

    add wandb_avg_drawer.py to draw expert avg return to wandb

---
+++ wandb_avg_drawer.py
@@ -0,0 +1,14 @@
+import wandb
+import argparse
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--group', action='store', type=str)
+    parser.add_argument('--avg', action='store', type=float)
+    parser.add_argument('--eps', action='store', type=int)
+    args = parser.parse_args()
+    group = args.group + '_{:.0f}'.format(args.avg)
+    wandb.init(anonymous='allow', project="FSRB", group=group, entity='cds')
+    for e in range(args.eps):
+        wandb.log({"reward": args.avg, "episode": e})
