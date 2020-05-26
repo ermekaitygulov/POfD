c4e32f6b507b45f2ddce9a1220e033c217d03fb1
ermekaitygulov <ermek.imankulov@gmail.com>
2020-05-26 20:37:55 +0300

    try to hardfix

--- wandb_avg_drawer.py
+++ wandb_avg_drawer.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     parser.add_argument('--eps', action='store', type=int)
     args = parser.parse_args()
     group = args.group + '_{:.0f}'.format(args.avg)
-    wandb.init(anonymous='allow', project="FSRB", group=group, entity='cds')
+    wandb.init(anonymous='allow', project="FSRB", group=group, entity='ermekaitygulov')
     for e in range(args.eps):
         wandb.log({"reward": args.avg, "episode": e})
